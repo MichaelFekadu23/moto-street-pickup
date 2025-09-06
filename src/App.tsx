b308cd5b6abb355926697edc1c6ab3b82046bbb4
Natnael <natnaelfekadu801@gmail.com>
2025-09-06 14:52:46 +0300

    ui fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import { useEffect } from 'react';
 import Layout from './components/Layout'; // Import the new Layout wrapper
 // import AwaitingDriverConfirm from './routes/AwaitingDriverConfirm';
-import Entry from './routes/Entry'; // Import the Entry component
+// import Entry from './routes/Entry'; // Import the Entry component
 // import Intrip from './routes/InTrip'; // Import the InTrip component
-// import Receipt from './routes/Receipt'; // Import the Receipt component
-// import TripComplete from './routes/TripComplete'; // Import the TripComplete component
+import Receipt from './routes/Receipt'; // Import the Receipt component
 
 function App() {
-  useEffect(() => {
-     const handleResize = () => {
-      document.body.style.height = `${window.innerHeight}px`;
-     };
-
-     window.addEventListener('resize', handleResize);
-     handleResize(); // Set initial height
-
-     return () => window.removeEventListener('resize', handleResize);
-    }, []);
   return (
     <Layout>
-      <Entry />
+      {/* <Entry /> */}
       {/* <AwaitingDriverConfirm /> */}
       {/* <Intrip /> */}
-      {/* <Receipt /> */}
-      {/* < TripComplete /> */}
+      <Receipt />
     </Layout>
   );
 }

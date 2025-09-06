76a7ec593b1fc8dc3fc7f898b00cea500d7a7a79
Natnael <natnaelfekadu801@gmail.com>
2025-09-06 13:00:40 +0300

    feat: intrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Layout from './components/Layout'; // Import the new Layout wrapper
-import AwaitingDriverConfirm from './routes/AwaitingDriverConfirm';
+// import AwaitingDriverConfirm from './routes/AwaitingDriverConfirm';
 // import Entry from './routes/Entry'; // Import the Entry component
+import Intrip from './routes/InTrip'; // Import the InTrip component
 
 function App() {
   return (
     <Layout>
       {/* <Entry /> */}
-      <AwaitingDriverConfirm />
+      {/* <AwaitingDriverConfirm /> */}
+      <Intrip />
     </Layout>
   );
 }
--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -7,7 +7,7 @@ interface PrimaryButtonProps {
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({ title }) => {
   return (
     <button className="relative w-full bg-black py-3 px-4 rounded-lg font-semibold text-white hover:bg-gray-800 transition-colors mt-6 flex items-center justify-center">
-      <span className="text-center">{title}</span>
+      <span className="text-center font-semibold text-[14px]">{title}</span>
       <span
         className="absolute right-4 top-1/2 -translate-y-1/2 text-xl font-semibold"
         aria-hidden="true"

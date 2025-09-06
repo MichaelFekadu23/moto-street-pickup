e5604602497038a88fb1b01e052e814ed92f9d77
Natnael <natnaelfekadu801@gmail.com>
2025-09-06 23:05:01 +0300

    ui fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col max-w-sm">
       <Header />
       {children}
     </div>

ddf14a258eadd71cb34973b56dd34bbc463e66dc
Natnael <natnaelfekadu801@gmail.com>
2025-09-06 13:27:39 +0300

    pay now

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Layout from './components/Layout'; // Import the new Layout wrapper
 // import AwaitingDriverConfirm from './routes/AwaitingDriverConfirm';
 // import Entry from './routes/Entry'; // Import the Entry component
-import Intrip from './routes/InTrip'; // Import the InTrip component
+// import Intrip from './routes/InTrip'; // Import the InTrip component
+import Receipt from './routes/Receipt'; // Import the Receipt component
 
 function App() {
   return (
     <Layout>
       {/* <Entry /> */}
       {/* <AwaitingDriverConfirm /> */}
-      <Intrip />
+      {/* <Intrip /> */}
+      <Receipt />
     </Layout>
   );
 }
---
+++ src/routes/Receipt.tsx
@@ -0,0 +1,56 @@
+import MainContentWrapper from '../components/MianContentWrapper';
+import PrimaryButton from '../components/PrimaryButton';
+import logo from '../assets/logo.svg';
+import Footer from '../components/Footer';
+import pay from '../assets/pay.svg';
+
+const Entry = () => {
+  return (
+    <MainContentWrapper>
+      <div className="flex flex-col items-center justify-between w-full h-full">
+
+        {/* Logo and Driver Info should take higher space */}
+        <div className="flex flex-col basis-2/5 gap-4 items-center justify-center w-full">
+          <div>
+          <img src={logo} alt="Logo" className="h-8 sm:h-12" />
+          </div>
+          <div className="text-white text-center">
+            <p className="font-semibold text-[24px] uppercase">
+              TRIPE
+            </p>
+            <p className="font-semibold text-[24px] uppercase">
+              COMPLETE
+            </p>
+          </div>
+        </div>
+
+        <div className="border border-white/50 p-4 rounded-md w-full max-w-sm mx-auto">
+          <div className="flex justify-between mb-4">
+            <span className="text-white font-normal text-[16px]">Distance :</span>
+            <span className="text-white">4.5 km</span>
+          </div>
+          <div className="flex justify-between mb-4">
+            <span className="text-white font-normal text-[16px]">Time :</span>
+            <span className="text-white">12 mins</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-white font-normal text-[16px]">Total :</span>
+            <span className="text-white">ETB 150</span>
+          </div>
+        </div>
+
+
+        {/* Buttons and Footer */}
+        <div className="flex flex-col w-full max-w-sm items-center justify-center gap-3 flex-shrink-0 mt-5">
+          <button className="relative w-full bg-black py-3 px-4 rounded-lg font-semibold text-white hover:bg-gray-800 transition-colors mt-6 flex items-center justify-center gap-3">
+            <img src={pay} alt="Pay Icon" className="h-5 w-5" />
+            <span className="text-center font-semibold text-[14px]">Pay Now</span>
+          </button>
+          <Footer text="Powered by Moto street pickup" />
+        </div>
+      </div>
+    </MainContentWrapper>
+  );
+};
+
+export default Entry;

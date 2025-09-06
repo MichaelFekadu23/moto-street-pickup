25a4b208cd118a692c26e3b72eab0d6a38b340b0
Natnael <natnaelfekadu801@gmail.com>
2025-09-06 22:56:50 +0300

    ui fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 
-// import Entry from './routes/Entry';
+import Entry from './routes/Entry';
 import AwaitingDriverConfirm from './routes/AwaitingDriverConfirm';
 import InTrip from './routes/InTrip';
 import TripComplete from './routes/TripComplete';
@@ -12,11 +12,12 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Receipt />} />
+          <Route path="/" element={<Entry />} />
           <Route path="/awaiting" element={<AwaitingDriverConfirm />} />
           <Route path="/trip" element={<InTrip />} />
           <Route path="/trip-complete" element={<TripComplete />} />
           <Route path="/receipt" element={<Receipt />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
     </Router>

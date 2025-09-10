362600cc64cc896b7f1b8a8a34f3ee2b204e23b8
Natnael <natnaelfekadu801@gmail.com>
2025-09-11 00:01:50 +0300

    gradient

--- src/components/MianContentWrapper.tsx
+++ src/components/MianContentWrapper.tsx
@@ -10,11 +10,10 @@ const MainContentWrapper: React.FC<MainContentWrapperProps> = ({ children }) => 
     <div
       className="relative flex-[0.65] flex flex-col items-center justify-between px-4 sm:px-6 md:px-8"
       style={{
-        backgroundImage: `url(${frame})`, // Replace '' with frame if you want to use the image
+        backgroundImage: `url(${frame})`,
         backgroundRepeat: 'repeat-x',
         backgroundSize: 'cover',
-        background: 'linear-gradient(180deg, rgb(16, 19, 28) 11.44%, rgba(255, 255, 255, 1) 157.97%)',
-        // mixBlendMode: 'luminosity'
+        background: 'linear-gradient(180deg, rgba(16, 19, 28, 0.8) 0%, rgba(16, 19, 28, 0) 11.44%), linear-gradient(180deg, rgb(16, 19, 28) 11.44%, rgba(255, 255, 255, 1) 127.97%)',
       }}
     >
       <div className='flex flex-col items-center gap-12 w-full max-w-sm '>
@@ -25,4 +24,3 @@ const MainContentWrapper: React.FC<MainContentWrapperProps> = ({ children }) => 
 };
 
 export default MainContentWrapper;
-

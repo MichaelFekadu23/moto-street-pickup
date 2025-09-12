c5c768838d6f20aee91c3786eaa7616701e610f7
Natnael <natnaelfekadu801@gmail.com>
2025-09-13 01:34:58 +0300

    ui fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import bgImg from '../assets/bgSvg.svg';
 
 const Header = () => {
   return (
-    <div className="relative w-full h-[44.44%] overflow-hidden z-10">
+    <div className="relative w-full h-[44.44%] sm:h-[53%] overflow-hidden z-10">
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{

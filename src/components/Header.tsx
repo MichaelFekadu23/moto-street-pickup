5d66f7e285ddb708a79a20a5d935690d4f1549ac
Natnael <natnaelfekadu801@gmail.com>
2025-09-12 23:05:32 +0300

    gradient

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,14 @@
-import bgImg from '../assets/bg-image.png';
+import bgImg from '../assets/bgSvg.svg';
 
 const Header = () => {
-  // A placeholder image URL is used since the original image asset is not available.
-  // const placeholderImageUrl = "https://placehold.co/1920x1080/0d0d0d/e5e7eb?text=Header+Image";
-  
   return (
-    <div className="relative w-full h-90 sm:h-96 overflow-hidden z-10">
+    <div className="relative w-full h-64 sm:h-80 md:h-96 overflow-hidden z-10">
       <div
         className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage: `url(${bgImg})`,
-        }}
+        style={{ backgroundImage: `url(${bgImg})`, backgroundColor: '#10141C' }}
       />
     </div>
   );
 };
-
 
 export default Header;

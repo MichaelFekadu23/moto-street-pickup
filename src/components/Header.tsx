f7f954ff9f58853936d136f1dd6045edd1f0d1d5
Natnael <natnaelfekadu801@gmail.com>
2025-09-13 00:43:28 +0300

    ui fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,15 @@ const Header = () => {
   return (
     <div className="relative w-full h-[44.44%] overflow-hidden z-10">
       <div
-      className="absolute inset-0 bg-cover bg-center"
-      style={{
-      zIndex: 10,
-      backgroundImage: `linear-gradient(to bottom, rgba(16, 20, 28, 0), rgba(16, 20, 28, 0.9) 110%), url(${bgImg})`,
-      backgroundColor: '#10141C',
-      }}
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          zIndex: 10,
+          backgroundImage: `linear-gradient(to bottom, rgba(16, 20, 28, 0.9) 0%, rgba(16, 20, 28, 0) 70%, rgba(16, 20, 28, 0.9) 100%), url(${bgImg})`,
+          backgroundColor: '#10141C',
+        }}
       />
     </div>
   );
 };
 
 export default Header;
-
-

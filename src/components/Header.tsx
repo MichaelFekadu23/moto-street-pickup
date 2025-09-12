e372698ee229fb9078d94d6311508444f3803d76
Natnael <natnaelfekadu801@gmail.com>
2025-09-13 00:00:48 +0300

    ui fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import bgImg from '../assets/bgSvg.svg';
 
 const Header = () => {
   return (
-    <div className="relative w-full h-70 sm:h-90 md:h-96 overflow-hidden z-10">
+    <div className="relative w-full h-100 overflow-hidden z-10">
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: `url(${bgImg})`, backgroundColor: '#10141C' }}
